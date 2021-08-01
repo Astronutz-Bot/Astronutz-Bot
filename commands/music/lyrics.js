579709f6fb26b020508502abcf5c3e9a661307ce
Atreya YT <74746579+diwasatreya@users.noreply.github.com>
2021-08-01 13:54:07 +0545

    Add files via upload

---
+++ commands/music/lyrics.js
@@ -0,0 +1,9 @@
+const discord = require("discord.js");
+module.exports = {
+  name: "lyrics", 
+  aliases: ["", "ly"], 
+  category: "<a:_NC_singerist2:853575942679822436> Music",
+  run: async (client, message, args) => {
+   
+}
+}
